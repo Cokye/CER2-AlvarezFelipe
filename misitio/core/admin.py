1f620a69a6317de70672f5748954eefba47d3b23
AlumnosInf <AlumnosInf@soportetiu.local>
2023-06-02 12:05:46 -0400

    Actualizacion

--- misitio/core/admin.py
+++ misitio/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import Comunicado,Categoria
 
 # Register your models here.
+
+class ComunicadoAdmin(admin.ModelAdmin):
+    pass
+
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Comunicado,ComunicadoAdmin)   # Permite gestionar la tabla carreras
+admin.site.register(Categoria,CategoriaAdmin)  
--- misitio/core/models.py
+++ misitio/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.nombre
+
+class Comunicado(models.Model):
+    NIVEL_CHOICES = [
+    ("GEN", "General"),
+    ("PRE", "Ciclo Preescolar"),
+    ("BAS", "Ciclo Basico"),
+    ("MED", "Ciclo Medio"),
+]
+    #identificador
+    titulo= models.CharField(max_length=30)
+    detalle= models.CharField(max_length=128)
+    nivel= models.CharField(max_length=3, choices=NIVEL_CHOICES)
+    fkcategoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_envio= models.DateField()
+    fecha_ultima= models.DateField()
+    user = get_object_or_404(User, nombre = request.user)
+    usuario= user
+
+    def __str__(self):
+        return self.titulo
