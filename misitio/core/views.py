1d0303a7b6324aba4d2cc5588caaf9b8eb865709
AlumnosInf <AlumnosInf@soportetiu.local>
2023-06-02 10:07:27 -0400

    Este es un mensaje 2

---
+++ misitio/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def home(request):
+    #return HttpResponse("<h1>Home<h1/>")
+    return render(request,'core/home.html')
